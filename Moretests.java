2b7c8bfea868888d0d78977e0d2a8ef6918963fb
Martin <l00099353@student.lyit.ie>
2016-11-28 18:02:24 +0000

    New repo for testing GitHub/Jenkins

---
+++ Moretests.java
@@ -0,0 +1,12 @@
+//author: Martin Redmond, l00099353
+//subject: Simple java file to test Git/Jenkins/GitHub
+//date: 28th Nov. 2016
+
+public class Moretests
+{
+	public static void main(String []args)
+	{
+		System.out.println("Just another output test 2016");
+	}
+	
+}
