e1cdd1ec6d9893b5439509a183ebb2dabd3187da
Martin <l00099353@student.lyit.ie>
2016-11-28 18:18:29 +0000

    Code edit to test Build trigger in Jenkins

--- Moretests.java
+++ Moretests.java
@@ -7,6 +7,11 @@ public class Moretests
 	public static void main(String []args)
 	{
 		System.out.println("Just another output test 2016");
+      //System.out.println("AND ANOTHER");
+         for (int i=0;i<=10;i++)
+         {
+            System.out.println("AND ANOTHER");
+         }
 	}
 	
 }
